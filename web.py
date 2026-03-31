a5b7112ca216e175a2119210b243d102bfbd30f5
hemerotecadetarifa-2 <98480086+hemerotecadetarifa-2@users.noreply.github.com>
2026-03-31 17:44:50 +0200

    Update web.py

--- web.py
+++ web.py
@@ -182,3 +182,6 @@ def run():
 # ------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
